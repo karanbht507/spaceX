8bb67f3e140be87bad38768d40b866c02d4c3b17
karanbht507 <karan.bhatia@ongraphmail.com>
2020-10-19 11:40:37 +0530

    Added footer

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,10 +6,11 @@ import "./home.scss";
 
 const Home = props => {
   return (
-    <div>
+    <>
       <h2>SpaceX Launch Programs</h2>
       <Landing {...props} />
-    </div>
+      <h5 className="footer">Developed by Karan Bhatia</h5>
+    </>
   );
 };
 
